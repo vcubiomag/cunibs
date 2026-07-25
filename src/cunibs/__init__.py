4251565e34670aeda14a1bcab46f17501da82ad4
Sriman Achanta <68172138+srimanachanta@users.noreply.github.com>
2026-07-25 11:22:24 -0400

    fix: define __version__

--- src/cunibs/__init__.py
+++ src/cunibs/__init__.py
@@ -1,5 +1,7 @@
 """GPU FEM solver for the TMS E-field."""
 
+from importlib.metadata import version
+
 from cunibs import coil, metrics
 from cunibs.coil import Coil
 from cunibs.mesh import HeadMesh, load_mesh
@@ -7,6 +9,8 @@ from cunibs.simulation import FieldResult, FieldSummary, Placement, Subject
 from cunibs import adm, uq
 from cunibs.adm import ResolvedTarget, Target, resolve_target
 from cunibs.uq import ConductivityUQConfig, ConductivityUQResult, ConductivityUQSummary
+
+__version__ = version("cunibs")
 
 __all__ = [
     "__version__",
